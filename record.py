885cd83b4374382a95bcade99a17e8b5f732a205
Igor Gala <igor.gala@ringteam.com>
2023-12-20 23:21:14 +0200

    add work with emails and addresses
    * implement user commands for work with emails and addresses
    * fix change_contact funtion to replace certain phone with new one
    * fix some minor error handlings

--- record.py
+++ record.py
@@ -5,36 +5,47 @@ class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
+        self.emails = []
+        self.address = []
 
     def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {', '.join(p.value for p in self.phones)}"
+        return (
+            f"Contact name: {self.name.value}\n"
+            f"phones: {', '.join(p.value for p in self.phones)}\n"
+            f"emails: {', '.join(e.value for e in self.emails)}\n "
+            f"address: {', '.join(a.value for a in self.address)}\n"
+        )
 
     def add_phone(self, phone):
-        if Phone.is_valid_phone(phone):
+        if Phone.is_valid(phone):
             self.phones.append(Phone(phone))
         else:
-            raise ValueError("Error: the phone number must be 10 digits")
+            raise PhoneValueError
 
     def remove_phone(self, phone):
         self.phones.remove(phone)
 
     def edit_phone(self, old_phone, new_phone):
         if Phone.is_valid_phone(new_phone):
-            for item in self.phones:
-                if item.value == old_phone:
-                    item.value = new_phone
-                    return
-            raise ValueError(
-                f"Error: the phone number {old_phone} not found in the record."
-            )
+            phone = self.find_phone(old_phone)
+            if phone:
+                phone.value = new_phone
+            else:
+                raise PhoneValueNotExist
         else:
-            raise ValueError("Error: the phone number must be 10 digits")
+            raise PhoneValueError
 
     def find_phone(self, phone):
-        for item in self.phones:
-            if item.value == phone:
-                return item.value
-        raise ValueError(f"Error: the phone number {phone} not found in the record.")
+        return next((item for item in self.phones if item.value == phone), None)
 
     def add_birthday(self, date):
         self.birthday = Birthday(date)
+
+    def add_email(self, email):
+        if Email.is_valid(email):
+            self.emails.append(Email(email))
+        else:
+            raise EmailValueError(email)
+
+    def add_address(self, address):
+        self.address.append(Address(address))
--- utils/cli_parse_decorator.py
+++ utils/cli_parse_decorator.py
@@ -17,20 +17,36 @@ class PhoneValueError(Exception):
     message = f"{error_msg} Phone number is not correct. Expected format is 10 digits."
 
 
+class PhoneValueNotExist(Exception):
+    def __init__(self, name, phone):
+        self.name = name
+        self.phone = phone
+        self.message = f"Contact {name} does not have saved phone number: {phone}"
+
+
+class EmailValueError(Exception):
+    def __init__(self, email):
+        self.email = email
+        self.message = f"Email {email} is not valid."
+
+
 def input_error(func):
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
 
+        # TODO: rewrite these Valuerror with more descriptive way, add name as paramter here
         except ValueError:
             return {
                 "add_contact": f"{error_msg} Use 'add [name] [phone number]'.",
-                "change_contact": f"{error_msg} Use 'change [name] [new phone number]'.",
+                "change_contact": f"{error_msg} Use 'change [name] [old phone number] [new phone number]'.",
                 "show_phone": f"{error_msg} Use 'phone [name]'.",
                 "show_all": f"{error_msg} Use 'all' without arguments.",
                 "parse_input": f"{error_msg} Use 'help' for commands list",
                 "add_birthday": f"{error_msg} Use 'add-birthday [name] [birtday]' birtday in format DD.MM.YYYY.",
                 "show_birthday": f"{error_msg} Use 'show-birthday [name]'.",
+                "show_email": f"{error_msg} Use 'show-email [name]'.",
+                "show_address": f"{error_msg} Use 'show-address [name]'.",
             }[func.__name__]
         except (
             RecordAlreadyExistsError,
