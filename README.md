db12440aa41044a3f6e1da37cb7eeeb8d9cc839b
Grigory Tereshko <gregory.tereshko1@gmail.com>
2023-12-20 23:29:52 +0200

    Show birthdays in N days

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ possible commands:
 'phone [name]' - show the phone of the user with entered name
 'add-birthday [name] [date]' - add birthday for name 'name' in format 'DD.MM.YYYY'
 'show-birthday[name]' - show birthday for name 'name'
+'show-birthday [name]' - show birthday for name 'name'
 'birthdays' - show all birthdays from the phone book on the next week
+'birthdays-in-days [days]' - show all birthdays in a particular amount of days
 'all' - print the contacnts phone book
 'close' or 'exit' - quit from the program
 'help' - print help message
--- next_week_birthdays.py
+++ next_week_birthdays.py
@@ -3,52 +3,45 @@ from collections import defaultdict
 
 
 def get_birthdays_per_week(users):
-    # 1. data preparation
-    birthdays_per_day = defaultdict(list)
-
-    # 2. obtaining the current date
+    birthdays = defaultdict(list)
     today = datetime.today().date()
+    next_saturday = today + timedelta(days=-today.weekday() + 5)
+    next_friday = next_saturday + timedelta(days=6)
 
-    # 3. users sorting
     for user in users:
-        # date conversion
-        name = user["name"]
-        birthday = user["birthday"].date()
-        # 4. evaluation of dates for this year
-        birthday_this_year = birthday.replace(year=today.year)
+      birthday = user["birthday"].date().replace(year=today.year)
+      # Since we move weekend birthdays to Monday let's start our "week" from saturday
+      if next_saturday <= birthday <= next_friday:
+        birthday_day_of_week = birthday.strftime("%A")
+        birthday_str = birthday.strftime("%d.%m.%Y")
+        if birthday.weekday() > 4: # Saturday & Sunday check
+          birthdays[f"Monday ({birthday_str})"].append(user["name"])
+        else:
+          birthdays[f"{birthday_day_of_week} ({birthday_str})"].append(user["name"])
 
-        if birthday_this_year < today:
-            birthday_this_year = birthday_this_year.replace(year=today.year + 1)
-
-        delta_days = (birthday_this_year - today).days
-
-        # evaluation for the next week
-        if delta_days < 7:
-            today_day_of_week = today.strftime("%A")
-            birthday_day_of_week = (today + timedelta(days=delta_days)).strftime("%A")
-
-            # corner case: skip birthdays of collegues with delta == 6 on the next Monday if today is Sunday
-            # (NOTE: this is not mentioned in task description, recomandation)
-            if today_day_of_week == "Sunday" and birthday_day_of_week == "Saturday":
-                continue
-
-            # if it's a weekend, postpone to Monday
-            if birthday_day_of_week in ["Saturday", "Sunday"]:
-                birthday_day_of_week = "Monday"
-            birthdays_per_day[birthday_day_of_week].append(name)
-
-    return "\n".join(
-        [f"{day}: {', '.join(names)}" for day, names in birthdays_per_day.items()]
-    )
+    for day, names in birthdays.items():
+      print(f"{day}: {', '.join(names)}")
 
 
-users = [
-    {"name": "Bill Gates", "birthday": datetime(1955, 10, 28)},
-    {"name": "Jill Valentine", "birthday": datetime(1974, 11, 30)},
-    {"name": "Kate Spade", "birthday": datetime(1962, 12, 23)},
-    {"name": "Yosyp Lozynskyi", "birthday": datetime(1807, 12, 20)},
-    {"name": "Kim Kardashian", "birthday": datetime(1980, 10, 21)},
-    {"name": "Ilon Mask", "birthday": datetime(1971, 6, 28)},
-]
+def get_birthdays_in_days(users, days_from_now = 0):
+    date = datetime.today().date() + timedelta(days=days_from_now)
+    birthday_day_of_week = date.strftime("%A")
+    birthday_str = date.strftime("%d.%m.%Y")
 
-get_birthdays_per_week(users)
+    birthday_users = list(filter(lambda user: (user['birthday'].date().replace(year=date.year) == date), users))
+    user_names = ', '.join(map(lambda user: user['name'], birthday_users))
+    print(f"{birthday_day_of_week} ({birthday_str}): {user_names}")
+
+
+# users = [
+#     {"name": "Bill Gates", "birthday": datetime(1955, 10, 28)},
+#     {"name": "Jill Valentine", "birthday": datetime(1974, 11, 30)},
+#     {"name": "Kate Spade", "birthday": datetime(1962, 12, 23)},
+#     {"name": "Yosyp Lozynskyi", "birthday": datetime(1807, 12, 27)},
+#     {"name": "Kim Kardashian", "birthday": datetime(1980, 10, 21)},
+#     {"name": "Ilon Mask", "birthday": datetime(1971, 6, 28)},
+# ]
+
+# get_birthdays_per_week(users)
+
+# get_birthdays_in_days(users, 4)
