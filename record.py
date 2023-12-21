00cd402ea6d7b70d7266a64c6d6c5639324c81b9
Grigory Tereshko <gregory.tereshko1@gmail.com>
2023-12-21 17:42:03 +0200

    Initialize birthday as None

--- record.py
+++ record.py
@@ -5,6 +5,7 @@ class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
+        self.birthday = None
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {', '.join(p.value for p in self.phones)}"
