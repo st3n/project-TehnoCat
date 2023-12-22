da30b9f8f51a63b7611c28bc9f08750bf9824164
gregorytereshko <gregory.tereshko1@gmail.com>
2023-12-22 23:28:03 +0200

    add feature command suggestion (#6)

--- src/assistant_bot_main.py
+++ src/assistant_bot_main.py
@@ -1,7 +1,8 @@
 import readline
 from src.phone_book import *
 from src.utils.cli_parse_decorator import *
-
+from src.command import bot_commands, find_closest_command, CommandCompleter, command_exists, find_command_by_name
+from prompt_toolkit import PromptSession
 
 @input_error
 def parse_input(user_input):
@@ -9,55 +10,13 @@ def parse_input(user_input):
     cmd = cmd.strip().lower()
     return cmd, *args
 
-
 def show_help():
     print("possible commands:")
-    print("'hello' - greetings message")
-    print("'add [name] [phone]' - add new contact in the phone book")
-    print("'change [name] [old_phone] [new_phone]' - change the saved contact phone")
-    print("'change [name] [old_email] [new_email]' - change the saved contact email")
-    print(
-        "'change [name] [old_address] | [new_address]' - change the saved contact address"
-    )
-    print("remove [name]' - remove contact")
-    print("remove [name] [phone]' - remove contact phone")
-    print("remove [name] [email]' - remove contact email")
-    print("remove [name] [address]' - remove contact address")
-    print("'phone [name]' - show the phone of the user with entered name")
-    print(
-        "'add-birthday [name] [date]' - add birthday for contact in format 'DD.MM.YYYY'"
-    )
-    print("add-email [name] [email] - add email for contact")
-    print("add-address [name] [address] - add address for contact")
-    print("show-email [name]")
-    print("show-address [name]")
-    print("show-birthday [name]")
-    print("'birthdays' - show all birthdays from the phone book on the next week")
-    print("'all' - print the contacnts phone book")
-    print("'close' or 'exit' - quit from the program")
-    print("'help' - print help message")
 
-
-command_dict = {
-    "add": add_contact,
-    "change": change_contact,
-    "remove": remove_contact,
-    "phone": show_phone,
-    "all": show_all,
-    "add-birthday": add_birthday,
-    "add-email": add_email,
-    "add-address": add_address,
-    "show-email": show_email,
-    "show-address": show_address,
-    "show-birthday": show_birthday,
-    "birthdays": show_birthdays_next_week,
-    "birthdays-in-days": show_birthdays_in_days,
-    'search-by-name': search_by_name,
-    'search-by-birthday': search_by_birthday,
-    'search-by-emails': search_by_emails,
-    'search-by-phones': search_by_phones
-}
-
+    for command_data in bot_commands():
+        name = f"{command_data['name']}"
+        args = f" [{', '.join(command_data['args'])}]" if command_data['args'] else ''
+        print(f"'{name}{args}' - {command_data['desc']}")
 
 def exit(history_file):
     readline.write_history_file(history_file)
@@ -67,6 +26,7 @@ def exit(history_file):
 def main():
     command_history = "../.command_history"
     contacts = AddressBook()
+    session = PromptSession(completer=CommandCompleter())
     print("Welcome to the assistant bot!")
 
     try:
@@ -76,7 +36,7 @@ def main():
 
     while True:
         try:
-            user_input = input("Enter a command: ").strip()
+            user_input = session.prompt('Enter a command: ').strip()
         except KeyboardInterrupt:
             exit(command_history)
             break
@@ -88,12 +48,15 @@ def main():
             break
         elif command == "hello":
             print("How can I help you?")
-        elif command in command_dict:
-            print(command_dict[command](args, contacts))
+        elif command_exists(command):
+            func_name = find_command_by_name(command)['func']
+            func = globals()[func_name]
+            print(func(args, contacts))
         elif command == "help":
             show_help()
         else:
             print("Invalid command.")
+            print(f"Did you mean '{find_closest_command(command)}'?\n")
             show_help()
 
 
